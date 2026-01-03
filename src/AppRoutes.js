209b6bc15f5adc6eead8c567dc3c5823565684cf
guidemdia <guidemdia@gmail.com>
2026-01-03 09:33:48 +0530

    Initial React Tailwind setup

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -1,7 +1,6 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import AddArticleForm from "./backend/AddArticleForm";
 import EditionForm from "./backend/EditionForm";
 import Separate from "./components/Separate";
 import SearchPage from "./pages/SearchPage";
@@ -9,7 +8,6 @@ import AboutPage from "./pages/AboutPage";
 import Login from "./components/Login";
 import IndividualPage from "./pages/IndividualPage";
 import EditionPage from "./pages/EditionPage";
-import AddMySpaceForm from "./backend/AddMySpaceForm";
 import MySpaceIndividual from "./pages/MySpaceIndividual";
 import MySpace from "./pages/MySpacePage";
 import Editing from "./backend/Editing";
@@ -19,38 +17,35 @@ import ArticlesPanel from "./backend/ArticlesPanel";
 
 export default function AppRoutes() {
   return (
-    <Router>
-      <Routes>
-        {/* Main Pages */}
-        <Route path="/" element={<Home />} />
-        <Route path="/search" element={<SearchPage />} />
-        <Route path="/about" element={<AboutPage />} />
-        
-        {/* Authentication */}
-        <Route path="/login" element={<Login />} />
-        
-        {/* Article Routes */}
-        <Route path="/articlespanel" element={<ArticlesPanel />} />
-        <Route path="/individual/:id" element={<IndividualPage />} />
-        <Route path="/category/:type" element={<Separate />} />
-        <Route path="/editing" element={<Editing />} />
-        
-        {/* MySpace Routes */}
-        <Route path="/myspace" element={<MySpace />} />
-        <Route path="/myspaceform" element={<MySpacePanel />} />
-        <Route path="/myspace/:id" element={<MySpaceIndividual />} />
-       
-          
-        {/* Edition Routes */}
-        <Route path="/editions" element={<EditionPage />} />
-        <Route path="/editionform" element={<EditionForm />} />
-        
-        {/* Admin Routes */}
-        <Route path="/admindashboard" element={<AdminDashboard />} />
-        
-        {/* Additional Routes */}
-        <Route path="/separate" element={<Separate />} />
-      </Routes>
-    </Router>
+    <Routes>
+      {/* Main Pages */}
+      <Route path="/" element={<Home />} />
+      <Route path="/search" element={<SearchPage />} />
+      <Route path="/about" element={<AboutPage />} />
+
+      {/* Authentication */}
+      <Route path="/login" element={<Login />} />
+
+      {/* Article Routes */}
+      <Route path="/articlespanel" element={<ArticlesPanel />} />
+      <Route path="/individual/:id" element={<IndividualPage />} />
+      <Route path="/category/:type" element={<Separate />} />
+      <Route path="/editing" element={<Editing />} />
+
+      {/* MySpace Routes */}
+      <Route path="/myspace" element={<MySpace />} />
+      <Route path="/myspaceform" element={<MySpacePanel />} />
+      <Route path="/myspace/:id" element={<MySpaceIndividual />} />
+
+      {/* Edition Routes */}
+      <Route path="/editions" element={<EditionPage />} />
+      <Route path="/editionform" element={<EditionForm />} />
+
+      {/* Admin Routes */}
+      <Route path="/admindashboard" element={<AdminDashboard />} />
+
+      {/* Additional Routes */}
+      <Route path="/separate" element={<Separate />} />
+    </Routes>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -15,7 +15,6 @@ export default function Home() {
          <Navbar />
       <Interface/>
         
-        
             <SliderL/>
                    <SliderR/>
             <Single/>
